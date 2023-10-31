00ffcd250b3d74fee32eb881b798cf4dcd2bb0e7
leafar300216 <baucor.rafael@gmail.com>
2023-10-31 14:59:16 +0100

    first commit

---
+++ src/main/java/com/not/test/git/NotTestGitApplication.java
@@ -0,0 +1,13 @@
+package com.not.test.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotTestGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotTestGitApplication.class, args);
+	}
+
+}
